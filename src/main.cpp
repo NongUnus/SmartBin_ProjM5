a3408ba4a6d4e90212ece253e3e3038701abdad6
NongUnus <sriwan98@gmail.com>
2026-01-21 21:05:55 +0700

    Update servo control and add required library

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
+#include <VL53L1X.h>
+#include <Servo.h>
+#include <Wire.h>
+VL53L1X sensor;
+Servo lid;
+const int servoPin = 5;
+const int openAngle = 90;
+const int closedAngle = 0;
+const int threshold = 100;
+void openLid() {
+    lid.write(openAngle);
+}
+void closeLid() {
+    lid.write(closedAngle);
+}
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    Wire.begin();
+    if(!sensor.init()) {
+        Serial.println("Failed to detect and initialize sensor!");
+        while(1);
+    }
+    sensor.setDistanceMode(VL53L1X::Short);
+    sensor.setMeasurementTimingBudget(50000);
+    sensor.startContinuous(50);
+    lid.attach(servoPin);
+    closeLid();
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    int distance = sensor.read();
+    if (distance == 0 || distance > 4000) {
+        Serial.println("Sensor Error / Out of Range");
+    }else {
+        Serial.print("Distance (mm): ");
+        Serial.println(distance);
+        if(distance < threshold) {
+            openLid();
+        }else {
+            closeLid();
+        }
+    }
+    delay(50);
 }
